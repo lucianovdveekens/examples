bc7a29e8cbabce44aad4b92355108150a9c66390
Luciano van der Veekens <lucianovdveekens@gmail.com>
2017-07-21 12:57:57 +0200

    Remove redundant path to feature file

--- testng/src/test/java/com/lvdveekens/examples/ApplesTest.java
+++ testng/src/test/java/com/lvdveekens/examples/ApplesTest.java
@@ -1,15 +1,15 @@
 package com.lvdveekens.examples;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-import static org.testng.AssertJUnit.assertEquals;
-
 @CucumberOptions(
-        features = {"classpath:com/lvdveekens/examples/Apples.feature", "classpath:features/Apples.feature"},
+        features = {"classpath:features/Apples.feature"},
         glue = "classpath:com.lvdveekens.examples",
         format = "pretty",
         strict = true
