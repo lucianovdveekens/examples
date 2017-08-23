e7613234f2c3ba6c950bcc818ab29e1ffbd81a62
Luciano van der Veekens <lucianovdveekens@gmail.com>
2017-08-23 19:49:37 +0200

    Alter the client code a bit

--- jersey/src/main/java/com/lvdveekens/examples/ExampleClient.java
+++ jersey/src/main/java/com/lvdveekens/examples/ExampleClient.java
@@ -1,6 +1,7 @@
 package com.lvdveekens.examples;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class ExampleClient {
@@ -9,7 +10,9 @@ public class ExampleClient {
         Client client = Client.create();
         WebResource webResource = client.resource("http://localhost:8081/api/example");
 
-        String response = webResource.type("application/json").get(String.class);
-        System.out.println(response);
+        ClientResponse clientResponse = webResource.type("application/json").get(ClientResponse.class);
+        System.out.println(clientResponse);
+        String entity = clientResponse.getEntity(String.class);
+        System.out.println(entity);
     }
 }
