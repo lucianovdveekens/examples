7acd3529355d8e8baacc357d3cbc2da3d7423577
Luciano van der Veekens <lucianovdveekens@gmail.com>
2017-10-04 19:42:09 +0200

    Add Spring Boot example

---
+++ springboot/src/main/java/com/lvdveekens/examples/springboot/services/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.lvdveekens.examples.springboot.services;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
